5c154225bc14002480c145aba3f62fe2042922df
DongJianHao123 <2995251733@qq.com>
2023-04-03 18:32:58 +0800

    link http->https

---
+++ _next/static/HPrPOS6lxTAISlwXs7_dV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/237-7c865bde554901f1.js","static/chunks/pages/index-e81dcc7a82b42c4b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/w8held7Q9KeIzj7A6jtzP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/237-7c865bde554901f1.js","static/chunks/pages/index-2a0b88010072b183.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
