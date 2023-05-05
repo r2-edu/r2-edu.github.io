afaed2d7472ee3239986ea0eb3250dacca47495b
DongJianHao123 <2995251733@qq.com>
2023-05-05 17:52:11 +0800

    fix: reduce clients for homepage

---
+++ _next/static/7dxqf9BjQAcJrNmkXYQ47/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/362-2b7ae503e0c210c1.js","static/chunks/pages/index-ec7df487f0a9a6e8.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/ROVaT6xcI6YNS0PCIabz1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/362-2b7ae503e0c210c1.js","static/chunks/pages/index-5cca3cdb33a7c9a2.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
